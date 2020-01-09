d1a13152b6080bd68a036643fb866d6716d29d89
Bouslog03 <bouslog@student.pcsstn.net>
2020-01-09 15:28:21 +0000

    numbers

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Ordinal numbers\n");
+  
+
+  unsigned int max;
+    printf("Max: ");
+    scanf("%u", &max);
+
+    for(int i = 1; i<= max; i++)
+    {
+      switch(i % 100){
+        case 11:
+        case 12:
+        case 13:
+        printf("%ith\n",i);
+        continue;
+      }
+      switch(i % 10)
+      {
+        case 1:
+          printf("%ist\n", i);
+        continue;
+         case 2:
+          printf("%ind\n", i);
+        continue;
+         case 3:
+          printf("%ird\n", i);
+        continue;
+         default:
+           printf("%ith\n", i);
+        continue;
+
+      }
+
+
+
+    }
   return 0;
-}
+} 
